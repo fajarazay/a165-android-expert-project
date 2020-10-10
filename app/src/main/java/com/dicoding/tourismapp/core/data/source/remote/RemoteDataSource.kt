a37b46eb0c2002c65fffe2bb21f17b4373420f45
Fajar Septian <fajarajay10@gmail.com>
2020-10-10 13:09:06 +0700

    Implementasi KOIN Dependency Injection

--- app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/RemoteDataSource.kt
@@ -9,15 +9,11 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
-class RemoteDataSource private constructor(private val apiService: ApiService) {
+class RemoteDataSource(private val apiService: ApiService) {
     companion object {
         @Volatile
         private var instance: RemoteDataSource? = null
 
-        fun getInstance(apiService: ApiService): RemoteDataSource =
-            instance ?: synchronized(this) {
-                instance ?: RemoteDataSource(apiService)
-            }
     }
 
     suspend fun getAllTourism(): Flow<ApiResponse<List<TourismResponse>>> {
