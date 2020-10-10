04fca9706894ddef9d2c6887f7992349c63f182b
Fajar Septian <fajarajay10@gmail.com>
2020-10-10 16:01:27 +0700

    Implementasi DAGGER Hilt

--- app/src/main/java/com/dicoding/tourismapp/core/di/DatabaseModule.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/di/DatabaseModule.kt
@@ -6,18 +6,23 @@ import com.dicoding.tourismapp.core.data.source.local.room.TourismDao
 import com.dicoding.tourismapp.core.data.source.local.room.TourismDatabase
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Singleton
 
 
 @Module
+@InstallIn(ApplicationComponent::class)
 class DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(context: Context): TourismDatabase = Room.databaseBuilder(
-        context,
-        TourismDatabase::class.java, "Tourism.db"
-    ).fallbackToDestructiveMigration().build()
+    fun provideDatabase(@ApplicationContext context: Context): TourismDatabase =
+        Room.databaseBuilder(
+            context,
+            TourismDatabase::class.java, "Tourism.db"
+        ).fallbackToDestructiveMigration().build()
 
     @Provides
     fun provideTourismDao(database: TourismDatabase): TourismDao = database.tourismDao()
